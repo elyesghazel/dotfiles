d7122e1f83e5bf3d27bc558c56889841fa9e3485
taaghel2 <elyes.ghazel@swisscom.com>
2026-03-04 13:49:40 +0100

    auto-sync: 2026-03-04 13:49

--- fish/.config/fish/conf.d/wsl.fish
+++ fish/.config/fish/conf.d/wsl.fish
@@ -9,8 +9,14 @@ abbr -a cdedu "cd $EDU_PATH"
 abbr -a cdplay "cd $PLAY_PATH"
 
 # Swisscom WSL Setup ---
-if test -f ~/.wsl4sc
-    bash -c "source ~/.wsl4sc >/dev/null; env" | grep -E '^[A-Za-z0-9_]+=.*$' | while read -l line
+if status is-interactive
+    and test -t 0
+    and not set -q FISH_WSL4SC_SHOWN
+    set -g FISH_WSL4SC_SHOWN 1
+
+    # Run ~/.wsl4sc *only* if terminal is interactive
+    # and skip if it's called from VS Code or pipe:
+    bash --login -i -c "source ~/.wsl4sc >/dev/null; env" | grep -E '^[A-Za-z0-9_]+=.*$' | while read -l line
         set name value (string split -m 2 '=' $line)
         set -gx $name $value
     end
