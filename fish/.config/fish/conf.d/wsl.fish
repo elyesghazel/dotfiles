dfd14d22d3a6c6995958c8f3eeb4d6bb7db071b1
taaghel2 <elyes.ghazel@swisscom.com>
2026-03-04 13:06:59 +0100

    auto-sync: 2026-03-04 13:06

--- fish/.config/fish/conf.d/wsl.fish
+++ fish/.config/fish/conf.d/wsl.fish
@@ -7,3 +7,11 @@ set -gx PLAY_PATH $HOME/projects/04_playground
 abbr -a cdbiz "cd $BIZ_PATH"
 abbr -a cdedu "cd $EDU_PATH"
 abbr -a cdplay "cd $PLAY_PATH"
+
+set -e HTTP_PROXY
+set -e HTTPS_PROXY
+set -e http_proxy
+set -e https_proxy
+set -e NO_PROXY
+set -e no_proxy
+
