3ae214e8239c4a78f82b59d7c3fa15ef8dc6fb31
eghazel.priv <elyes@elyesghazel.ch>
2026-04-09 07:01:51 +0200

    auto-sync []: added commit msg func to dotsync fish function – 2026-04-09 07:01

--- fish/.config/fish/functions/dotsync.fish
+++ fish/.config/fish/functions/dotsync.fish
@@ -1,26 +1,19 @@
 # 1. sync everything to github
 function dotsync
     set -l dotpath "$HOME/dotfiles"
+    set -l current_time (date "+%Y-%m-%d %H:%M")
+    set -l host (hostname)
 
-    # update package lists first
     bash $dotpath/update_pkg.sh
-
     cd $dotpath
     git add .
 
-    # get current timestamp
-    set -l current_time (date "+%Y-%m-%d %H:%M")
-
-    # detect system for commit message
-    if set -q OS_KIND
-        set -l system_name (string upper $OS_KIND)
-        git commit -m "auto-sync [$system_name]: $current_time"
+    if set -q argv[1]
+        git commit -m "auto-sync [$host]: $argv[1] – $current_time"
     else
-        # fallback if OS_KIND is not set
-        git commit -m "auto-sync: $current_time"
+        git commit -m "auto-sync [$host]: $current_time"
     end
 
     git push origin main
-
     echo "Everything synced to GitHub!"
 end
