c81bd341f58cdf8d14fb6495a22c030f357a724a
eghazel.priv <elyes@elyesghazel.ch>
2026-04-09 07:02:42 +0200

    auto-sync [elyes-laptop]: fix: dotsync function hostname – 2026-04-09 07:02

--- fish/.config/fish/functions/dotsync.fish
+++ fish/.config/fish/functions/dotsync.fish
@@ -2,7 +2,7 @@
 function dotsync
     set -l dotpath "$HOME/dotfiles"
     set -l current_time (date "+%Y-%m-%d %H:%M")
-    set -l host (hostname)
+    set -l host (uname -n)
 
     bash $dotpath/update_pkg.sh
     cd $dotpath
